72f87d51cdd33e6ed480f8d2ea2014839da5d75d
EmilceAide <emilceaide.t@gmail.com>
2023-04-26 12:06:57 -0500

    components: Nav, Card and Card Container

---
+++ src/components/CardsContainer/CardsContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CardsContainer = () => {
+    return (
+        <div>
+            <p>Cards Container</p>
+        </div>
+    );
+}
+
+export default CardsContainer;
