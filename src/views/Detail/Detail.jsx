10378e80a122433b0d02b6fc63ec3cb7db48eee6
EmilceAide <emilceaide.t@gmail.com>
2023-04-26 13:15:24 -0500

    detail structure was created

--- src/views/Detail/Detail.jsx
+++ src/views/Detail/Detail.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const Detail = () => {
+const Detail = ({ name, image, hp, attack, defense, speed, height,
+weight, types }) => {
     return (
         <div>
-            <p>Aquí se veran los detalles de un pokemon</p>
+            <h3>Nombre: {name} </h3>
+            <img src={image} alt="Avatar del pokemón" />
+            <p>Vida: {hp} </p>
+            <p>Ataque: {attack} </p>
+            <p>Defensa: {defense} </p>
+            <p>Velocidad: {speed} </p>
+            <p>Altura: {height} </p>
+            <p>Peso: {weight} </p>
+            <p>Tipo: {types} </p>
         </div>
     );
 }
