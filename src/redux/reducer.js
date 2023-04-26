7ca3f66ec9572dd074c6eda945e34266eb01e565
EmilceAide <emilceaide.t@gmail.com>
2023-04-26 18:48:25 -0500

    reducer: GET_POKEMONS, GET_POKEMON_ID, GET_TYPES

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,13 +1,26 @@
+import { GET_POKEMONS,  GET_POKEMON_ID, GET_TYPES } from "./actions-types";
+
 const initialState = {
+  pokemons: [],
+  pokemonId: [],
+  pokemonTypes: [],
 
   };
   
-  const reducer = (state = initialState, action) => {
+  const rootReducer = (state = initialState, action) => {
     switch (action.type) {
+      case GET_POKEMONS:
+        return {...state, pokemons: action.payload};
+
+      case  GET_POKEMON_ID:
+        return {pokemonId: action.payload};
+        
+      case GET_TYPES:
+        return {...state, pokemonTypes: action.payload};
       
       default:
         return { ...state };
     }
   };
   
-  export default reducer;
+  export default rootReducer;
