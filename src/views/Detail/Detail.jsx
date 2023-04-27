558d5bc4f74a398f8fa1d4b73dd4317c8a2322f3
EmilceAide <emilceaide.t@gmail.com>
2023-04-27 02:43:23 -0500

    key

--- src/views/Detail/Detail.jsx
+++ src/views/Detail/Detail.jsx
@@ -2,13 +2,12 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { getPokemon } from "../../redux/actions";
-import Card from '../../components/Card/Card'
+import Card from "../../components/Card/Card";
 
 const Detail = () => {
-
   let detail = true;
   const { pokemonId } = useSelector((state) => state);
- 
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getPokemon());
@@ -16,20 +15,23 @@ const Detail = () => {
 
   return (
     <div>
-      {pokemonId.map(pokemon =>{
-      return (<Card
-        id={pokemon.id}
-        name={pokemon.name}
-        image={pokemon.image}
-        hp={pokemon.hp}
-        attack={pokemon.attack}
-        defense={pokemon.defense}
-        speed={pokemon.speed}
-        height={pokemon.height}
-        weight={pokemon.weight}
-        types={pokemon.types}
-        detail={detail}
-      /> )
+      {pokemonId.map((pokemon) => {
+        return (
+          <Card
+            key={pokemon.id}
+            id={pokemon.id}
+            name={pokemon.name}
+            image={pokemon.image}
+            hp={pokemon.hp}
+            attack={pokemon.attack}
+            defense={pokemon.defense}
+            speed={pokemon.speed}
+            height={pokemon.height}
+            weight={pokemon.weight}
+            types={pokemon.types}
+            detail={detail}
+          />
+        );
       })}
     </div>
   );
