e6b66d1c131ec99af5b831bb345deff70d0e8d13
EmilceAide <emilceaide.t@gmail.com>
2023-04-27 02:42:44 -0500

    form

--- src/views/Create/Create.jsx
+++ src/views/Create/Create.jsx
@@ -1,27 +1,81 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { getTypes } from "../../redux/actions";
 
 const Create = () => {
+  const { pokemonTypes } = useSelector((state) => state);
 
-    const { pokemonTypes } = useSelector((state) => state);
- 
-    const dispatch = useDispatch();
-    useEffect(() => {
-      dispatch(getTypes());
-    }, []);
+  const form ={
 
-    return (
+  }
+
+  const changeHandler = () =>{
+
+  }
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getTypes());
+  }, []);
+
+  return (
+    <div>
+      <p>Formulario para crear pokemones</p>
+      <form>
         <div>
-            <p>Formulario para crear pokemones</p>
-            {pokemonTypes.map(type => {
-                return (
-                    <h5> Tipo: {type.name}</h5>
-                )
-            })}
+          <label>Nombre: </label>
+          <input type="text" value={form.name} onChange={changeHandler} />
         </div>
-    );
-}
+
+        <div>
+          <label>Imagen: </label>
+          <input type="text" value={form.image} onChange={changeHandler} />
+        </div>
+
+        <div>
+          <label>hp: </label>
+          <input type="number"  value={form.hp} onChange={changeHandler} />
+        </div>
+
+        <div>
+          <label>Ataque: </label>
+          <input type="number"  value={form.attack} onChange={changeHandler} />
+        </div>
+
+        <div>
+          <label>Defensa: </label>
+          <input type="number"  value={form.defense} onChange={changeHandler} />
+        </div>
+
+        <div>
+          <label>Velocidad: </label>
+          <input type="number"  value={form.speed} onChange={changeHandler} />
+        </div>
+
+        <div>
+          <label>Altura: </label>
+          <input type="number"  value={form.height} onChange={changeHandler} />
+        </div>
+
+        <div>
+          <label>Peso: </label>
+          <input type="number"  value={form.weigth} onChange={changeHandler} />
+        </div>
+
+
+        <div>
+          {pokemonTypes.map((type) => {
+            return (
+                <div key={type.id}>
+                    <h5>{type.name}</h5>
+                </div>
+            )
+          })}
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Create;
