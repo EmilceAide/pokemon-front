1885aeda001747e9516059f459608db48a9dd4c9
EmilceAide <emilceaide.t@gmail.com>
2023-05-02 20:05:38 -0500

    show only one section of type

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,35 +1,43 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 
-import styles from './card.module.css'
+import styles from "./card.module.css";
 
-const Card = ({id, name, image, hp, attack, defense, speed, height, weight, Types, types, detail}) => {
+const Card = ({ id, name, image, hp, attack, defense, speed, height, weight, Types, types, detail}) => {
 
+  let arrType = [];
+  if (types && !Types) {
+    arrType = typeof types === 'string' ? types.split(',') : [];
+  }
+  if (Types) {
+    Types?.map((type) => arrType.push(type.name));
+  }
 
-    return (
+  return (
+    <div className={styles.container}>
+      {detail && <h3 className={styles.id}>Id: {id}</h3>}
+      <img src={image} alt="Avatar del pokemón" className={styles.image} />
+      <h3 className={styles.name}>{name} </h3>
+      {detail && (
         <div>
-            <h3>Id: {id}</h3>
-            <h3>Nombre: {name} </h3>
-            <img src={image} alt="Avatar del pokemón" />
-            {detail && (
-              <div>
-              <p>Vida: {hp} </p>
-              <p>Ataque: {attack} </p>
-              <p>Defensa: {defense} </p>
-              <p>Velocidad: {speed} </p>
-              <p>Altura: {height} </p>
-              <p>Peso: {weight} </p>
-              </div>
-          )}
-            <p>Tipo: {types} </p>
-            <p>Tipo: {Types?.map(type=> `${type.name} `)}</p>
-            {!detail && (
-            <Link to={`/pokemon/${id}`}>
-            <button >{` Ver más + `} </button>
-            </Link>
-            )}
+          <p className={styles.hp}>Vida: {hp} </p>
+          <p className={styles.attack}>Ataque: {attack} </p>
+          <p className={styles.defense}>Defensa: {defense} </p>
+          <p className={styles.speed}>Velocidad: {speed} </p>
+          <p className={styles.height}>Altura: {height} </p>
+          <p className={styles.weight}>Peso: {weight} </p>
         </div>
-    );
-}
+      )}
+      {arrType.map((el) => {
+        return (<p>Tipo: {el}</p>)
+      })}
+      {!detail && (
+        <Link to={`/pokemon/${id}`}>
+          <button>{` Ver más + `} </button>
+        </Link>
+      )}
+    </div>
+  );
+};
 
 export default Card;
