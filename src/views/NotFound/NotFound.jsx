2af6363ed3eaf738c8f9bb1dbc7bc3013fd8e4ed
EmilceAide <emilceaide.t@gmail.com>
2023-05-06 00:37:44 -0500

    added image and styles to NotFound

--- src/views/NotFound/NotFound.jsx
+++ src/views/NotFound/NotFound.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useLocation } from "react-router-dom";
+
 import styles from './NotFound.module.css';
+import image from '../../assets/interno.gif'
 
 const NotFound = () => {
 
@@ -10,6 +12,7 @@ const NotFound = () => {
     <div className={styles.container}>
       <h1>404 - Página no encontrada</h1>
       <p>La ruta {`http://localhost:3000${location.pathname}`} no existe</p>
+      <img src={image} alt='Imagen de Pikachu' />
     </div>
   );
 };
